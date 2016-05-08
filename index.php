b8b1ac96f36bb29c60eb612b7cfaf7970ed29403
Moulik Adak <Moulik.adak@gmail.com>
2016-05-08 21:00:59 +0530

    Added oauth to main page

--- index.php
+++ index.php
@@ -3,9 +3,28 @@
 	<title>CareODrive</title>
 	<link rel="stylesheet" href="style/css/bootstrap.min.css" type="text/css" />
 	<script type="text/javascript" src="style/js/bootstrap.js" ></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <script>
+        var OAUTHURL    =   'https://accounts.google.com/o/oauth2/auth?';
+        var VALIDURL    =   'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=';
+        var SCOPE       =   'https://www.googleapis.com/auth/userinfo.profile';
+        var CLIENTID    =   '433322211111.apps.googleusercontent.com';
+        var REDIRECT    =   'http://localhost:8888/MAMP/html5/oauth/'
+        var TYPE        =   'token';
+        var _url        =   OAUTHURL + 'scope=' + SCOPE + '&client_id=' + CLIENTID + '&redirect_uri=' + REDIRECT + '&response_type=' + TYPE;
+        function login() {
+
+        }
+    </script>
 </head>
 <body>
+
 <h1 align="center"> Welcome To CareOdrive.com !!!</h1>
+
+<a href='#' onClick='login();'> click here to login </a>
+    <div id='uName'>Welcome  </div>
+    <img src='' id='imgHolder'/>
+
    <div class="container">
     <div class="btn-group btn-group-justified">
     <a href="fb.php" class="btn btn-primary">Facebook Login</a>
